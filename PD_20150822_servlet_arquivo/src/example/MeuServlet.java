400f332f4a66d6c1ed9a88119d05c47949c94fc0
Walter Gandarella <walter.wgbn@gmail.com>
2015-08-24 19:18:26 -0300

    20150824

---
+++ PD_20150822_servlet_arquivo/src/example/MeuServlet.java
@@ -0,0 +1,16 @@
+package example;
+
+import java.io.IOException;
+
+/**
+ * Created by walter on 22/08/15.
+ */
+public class MeuServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
