b6e0ef1cc4145d815eaba70d9af320c724cb241c
Walter Gandarella <walter.wgbn@gmail.com>
2015-09-15 08:33:31 -0300

    ViewScoped

--- PD_20150905_jsf_produto/src/java/ProdutoBean.java
+++ PD_20150905_jsf_produto/src/java/ProdutoBean.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean(name = "prodBean")
-@SessionScoped
+@ViewScoped
 public class ProdutoBean {
     private List<Produto> produtos;
     private Produto produto;
